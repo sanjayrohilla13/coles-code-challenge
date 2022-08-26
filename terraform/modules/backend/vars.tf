156deb66c8a9b3663893d1f9f77aaa995731f515
sanjay <sanjayrohilla@gmail.com>
2022-08-26 12:01:19 +1000

    initial repo

---
+++ terraform/modules/backend/vars.tf
@@ -0,0 +1,11 @@
+variable "aws_acc_no" {
+    description = "AWS Account Number for deploying the application"
+}
+
+variable "env" {
+    description = "Environment Name"
+}
+
+variable "app_name" {
+  description = "Application Name"
+}
